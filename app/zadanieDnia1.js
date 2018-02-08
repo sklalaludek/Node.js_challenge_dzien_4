9d3fc240cc65212296ddf9ea1d89211a88815d0b
sklalaludek <pattiegolis@gmail.com>
2018-02-08 20:15:02 +0100

    zadanie dnia 4 - 1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,11 @@
 //Twój kod
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.end('Hello, World from back-end!');
+});
+
+server.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
